4f59f5cdeac5ca2cfb35db11da55529fae9555d5
Le Yang <yangle0125@qq.com>
2018-03-04 13:18:08 +0800

    fix some error

--- layer/mse_layer.c
+++ layer/mse_layer.c
@@ -3,8 +3,22 @@
 
 static void mse_layer_prepare(layer_t *l)
 {
-	l->out.size = 1;
-	l->param.size = l->n->batch;
+	if (l->in.size != 0)
+	{
+		l->param.size = l->in.size;
+	}
+
+	if (l->out.size != 1)
+	{
+		l->out.size = 1;
+	}
+
+	if (l->param.size != 0)
+	{
+		l->in.size = l->param.size;
+	}
+
+	l->param.size *= l->n->batch;
 
 	LOG("mse_layer: in %d, out %d, param %d\n", l->in.size, l->out.size, l->param.size);
 }
@@ -18,9 +32,9 @@ static void mse_layer_forward(layer_t *l)
 		l->out.val[b] = 0;
 		for (i = 0; i < l->in.size; ++i)
 		{
-			l->out.val[b] += (l->in.val[b * l->in.size + i] - l->param.val[b]) * (l->in.val[b * l->in.size + i] - l->param.val[b]) / 2;
+			l->out.val[b] += (l->in.val[b * l->in.size + i] - l->param.val[b * l->in.size + i]) * (l->in.val[b * l->in.size + i] - l->param.val[b * l->in.size + i]) / 2;
 		}
-	}	
+	}
 }
 
 static void mse_layer_backward(layer_t *l)
@@ -28,19 +42,18 @@ static void mse_layer_backward(layer_t *l)
 	int i = 0, b = 0;
 
 	for (b = 0; b < l->n->batch; ++b)
-	for (i = 0; i < l->in.size; ++i)
-	{
-		l->in.grad[b * l->in.size + i] = l->out.grad[b] * (l->in.val[b * l->in.size + i] - l->param.val[b]);
-	}
+		for (i = 0; i < l->in.size; ++i)
+		{
+			l->in.grad[b * l->in.size + i] = l->out.grad[b] * (l->in.val[b * l->in.size + i] - l->param.val[b * l->in.size + i]);
+		}
 }
 
 static const layer_func_t mse_func = {
 	mse_layer_prepare,
 	mse_layer_forward,
-	mse_layer_backward
-};
+	mse_layer_backward};
 
-layer_t* mse_layer(int in, int out, int param)
+layer_t *mse_layer(int in, int out, int param)
 {
 	layer_t *l = layer(in, out, param, &mse_func);
 
--- layer/softmax_layer.c
+++ layer/softmax_layer.c
@@ -25,11 +25,12 @@ static void softmax_layer_prepare(layer_t *l)
 static void softmax_layer_forward(layer_t *l)
 {
 	int i = 0, b = 0;
-	float sum = 0;
-	float max = l->in.val[0];
 
 	for (b = 0; b < l->n->batch; ++b)
 	{
+		float sum = 0;
+		float max = l->in.val[b * l->in.size];
+
 		// see http://freemind.pluskid.org/machine-learning/softmax-vs-softmax-loss-numerical-stability
 		for (i = 1; i < l->in.size; ++i)
 		{
@@ -55,25 +56,24 @@ static void softmax_layer_backward(layer_t *l)
 	int i = 0, b = 0;
 
 	for (b = 0; b < l->n->batch; ++b)
-	for (i = 0; i < l->in.size; ++i)
-	{
-		int o = 0;
-
-		l->in.grad[b * l->in.size + i] = 0;
-		for (o = 0; o < l->out.size; ++o)
+		for (i = 0; i < l->in.size; ++i)
 		{
-			l->in.grad[b * l->in.size + i] += l->out.grad[b * l->out.size + o] * l->out.val[b * l->out.size + o] * ((i == o) - l->out.val[b * l->out.size + i]);
+			int o = 0;
+
+			l->in.grad[b * l->in.size + i] = 0;
+			for (o = 0; o < l->out.size; ++o)
+			{
+				l->in.grad[b * l->in.size + i] += l->out.grad[b * l->out.size + o] * l->out.val[b * l->out.size + o] * ((i == o) - l->out.val[b * l->out.size + i]);
+			}
 		}
-	}
 }
 
 static const layer_func_t softmax_func = {
 	softmax_layer_prepare,
 	softmax_layer_forward,
-	softmax_layer_backward
-};
+	softmax_layer_backward};
 
-layer_t* softmax_layer(int in, int out, int param)
+layer_t *softmax_layer(int in, int out, int param)
 {
 	layer_t *l = layer(in, out, param, &softmax_func);
 
